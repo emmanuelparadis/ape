f599b0a93a1046f70b2a42299251cf76f45f4b73
SMITH <pjjg18@durham.ac.uk>
2022-01-12 14:01:17 +0000

    More tests & fixes

--- tests/testthat/test-summary.phylo.R
+++ tests/testthat/test-summary.phylo.R
@@ -17,8 +17,28 @@ test_that("[<-.multiPhylo works", {
   unlink(nexFile) # delete temporary file
   
   # trees read from a nexus file have a $TipLabel attribute
-  expect_equal(length(attributes(fromNexus)$TipLabel), 6)
-  attr(fromNexus, "names") = NULL
-  fromNexus[] <- twoTrees
-  expect_equal(fromNexus, twoTrees)
+  tipLabel <- attr(fromNexus, "TipLabel")
+  expect_equal(length(tipLabel), 6)
+  attr(fromNexus, "names") <- NULL
+  withoutTL <- c(fromNexus[[1]], fromNexus[[2]])
+  bothLabels <- structure(withoutTL, TipLabel = tipLabel)
+  
+  trees <- fromNexus
+  trees[] <- fromNexus
+  expect_equal(trees, fromNexus)
+  
+  trees[] <- fromNexus[2:1]
+  expect_equal(trees, rev(fromNexus))
+  
+  trees[1] <- trees[2]
+  expect_equal(trees, fromNexus[c(1, 1)])
+  Capitalize <- function (tree) {
+    tree$tip.label <- toupper(tree$tip.label)
+    # Return:
+    tree
+  }
+  trees <- fromNexus
+  trees[] <- lapply(trees, Capitalize)
+  expect_equal(trees,
+               structure(lapply(fromNexus, Capitalize), class = 'multiPhylo'))
 })
