17b367b9816fdc626b5b5cd4d91c339a657d81b9
Klaus Schliep <klaus.schliep@gmail.com>
2022-04-24 20:03:13 +0200

    remove recursions

--- R/balance.R
+++ R/balance.R
@@ -11,21 +11,20 @@ balance <- function(phy)
 {
     if (!inherits(phy, "phylo"))
       stop('object "phy" is not of class "phylo"')
-    phy <- reorder(phy) # fix a bug reported by G. Valiente in January 2009 (2015-07-08)
+    phy <- reorder(phy, "postorder")
     N <- length(phy$tip.label)
     nb.node <- phy$Nnode
     if (nb.node != N - 1)
       stop('"phy" is not rooted and fully dichotomous')
     ans <- matrix(NA, nb.node, 2)
-    foo <- function(node, n) {
-        s <- which(phy$edge[, 1] == node)
-        desc <- phy$edge[s, 2]
-        ans[node - N, 1] <<- n1 <- (s[2] - s[1] + 1)/2
-        ans[node - N, 2] <<- n2 <- n - n1
-        if (desc[1] > N) foo(desc[1], n1)
-        if (desc[2] > N) foo(desc[2], n2)
+    nd <- node.depth(phy)
+    i <- 1
+    while(i < nrow(phy$edge)){
+        node <- phy$edge[i, 1] - N
+        ans[node, 1] <- nd[phy$edge[i,2]]
+        ans[node, 2] <- nd[phy$edge[i+1,2]]
+        i <- i+2
     }
-    foo(N + 1, N)
     rownames(ans) <-
       if (is.null(phy$node.label)) N + 1:nb.node else phy$node.label
     ans
--- R/ladderize.R
+++ R/ladderize.R
@@ -9,34 +9,53 @@
 
 ladderize <- function(phy, right = TRUE)
 {
-    foo <- function(node, END, where) {
-        start <- which(phy$edge[, 1] == node)
-        end <- c(start[-1] - 1, END)
-        size <- end - start + 1
-        desc <- phy$edge[start, 2]
-        Nclade <- length(desc)
-        n <- N[desc]
-        o <- order(n, decreasing = right)
-        newpos <- c(0, cumsum(size[o][-Nclade])) + where
-        desc <- desc[o]
-        end <- end[o]
-        start <- start[o]
-        neworder[newpos] <<- start
-        for (i in 1:Nclade)
-            if (desc[i] > nb.tip) foo(desc[i], end[i], newpos[i] + 1)
+    desc_fun <- function(x) {
+        parent <- x[, 1]
+        children <- x[, 2]
+        res <- vector("list", max(x))
+        for (i in seq_along(parent)) res[[parent[i]]] <- c(res[[parent[i]]], children[i])
+        return(res)
     }
-    phy <- reorder(phy) # fix by Klaus (2015-10-04)
+
+    if(!is.null(phy$edge.length)){
+        el <- numeric(max(phy$edge))
+        el[phy$edge[, 2]] <- phy$edge.length
+    }
+
     nb.tip <- length(phy$tip.label)
     nb.node <- phy$Nnode
     nb.edge <- dim(phy$edge)[1]
-    tmp <- reorder(phy, "postorder")
+
+    phy <- reorder(phy, "postorder")
     N <- .C(node_depth, as.integer(nb.tip),
-            as.integer(tmp$edge[, 1]), as.integer(tmp$edge[, 2]),
+            as.integer(phy$edge[, 1]), as.integer(phy$edge[, 2]),
             as.integer(nb.edge), double(nb.tip + nb.node), 1L)[[5]]
-    neworder <- integer(nb.edge)
-    foo(nb.tip + 1, nb.edge, 1)
-    phy$edge <- phy$edge[neworder, ]
-    if (!is.null(phy$edge.length))
-        phy$edge.length <- phy$edge.length[neworder]
+
+    ii <- order(x <- phy$edge[,1], y <- N[phy$edge[,2]], decreasing = right)
+    desc <- desc_fun(phy$edge[ii,])
+
+    tmp <- integer(nb.node)
+    new_anc <- integer(nb.node)
+    new_anc[1] <- tmp[1] <- nb.tip + 1L
+    k <- nb.node
+    pos <- 1L
+
+    while(pos > 0L && k > 0){
+        current <- tmp[pos]
+        new_anc[k] <- current
+        k <- k - 1L
+        dc <- desc[[current]]
+        ind <- (dc > nb.tip)
+        if(any(ind)){
+            l <- sum(ind)
+            tmp[pos -1L + seq_len(l)] <-  dc[ind]
+            pos <- pos + l - 1L
+        }
+        else pos <- pos - 1L
+    }
+    edge <- cbind(rep(new_anc, lengths(desc[new_anc])), unlist(desc[new_anc]))
+    phy$edge <- edge
+    if(!is.null(phy$edge.length)) phy$edge.length <- el[edge[,2]]
+    attr(phy, "order") <- "postorder"
     phy
 }
