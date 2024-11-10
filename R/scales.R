570cae63e5755707e5bb4a51f580099810f701fe
Klaus Schliep <klaus.schliep@gmail.com>
2024-11-10 12:16:11 +0100

    improve axisPhylo, solving issue #116 (axisPhylo() not extending all along the root edge). side is detected from plot.

--- R/scales.R
+++ R/scales.R
@@ -72,72 +72,91 @@ add.scale.bar <- function(x, y, length = NULL, ask = FALSE,
            })
 }
 
-axisPhylo <- function(side = 1, root.time = NULL, backward = TRUE, ...)
+axisPhylo <- function(side = NULL, root.time = NULL, backward = TRUE, ...)
 {
-    lastPP <- get("last_plot.phylo", envir = .PlotPhyloEnv)
-    type <- lastPP$type
-
-    if (type == "unrooted")
-        stop("axisPhylo() not available for unrooted plots; try add.scale.bar()")
-    if (type == "radial")
-        stop("axisPhylo() not meaningful for this type of plot")
-
-    if (is.null(root.time)) root.time <- lastPP$root.time
-
-    if (type %in% c("phylogram", "cladogram")) {
-        xscale <-
-            if (lastPP$direction %in% c("rightwards", "leftwards")) range(lastPP$xx)
-            else range(lastPP$yy)
-
-        tmp <- lastPP$direction %in% c("leftwards", "downwards")
-
-        tscale <- c(0, xscale[2] - xscale[1])
-        if (xor(backward, tmp)) tscale <- tscale[2:1]
-        if (!is.null(root.time)) {
-            tscale <- tscale + root.time
-            if (backward) tscale <- tscale - xscale[2]
-        }
-
-        ## the linear transformation between the x-scale and the time-scale:
-        beta <- diff(xscale) / diff(tscale)
-        alpha <- xscale[1] - beta * tscale[1]
-
-        lab <- pretty(tscale)
-        x <- beta * lab + alpha
-        axis(side = side, at = x, labels = lab, ...)
-    } else { # type == "fan"
-        n <- lastPP$Ntip
-        xx <- lastPP$xx[1:n]; yy <- lastPP$yy[1:n]
-        r0 <- max(sqrt(xx^2 + yy^2))
-
-        ## find the widest angle between tips:
-        alpha <- sort(setNames(rect2polar(xx, yy)$angle, 1:n)) # from -pi to +pi
-        angles <- c(diff(alpha), 2*pi - alpha[n] + alpha[1L])
-        j <- which.max(angles)
-        i <- if (j == 1L) n else j - 1L # this is a circle...
-        firstandlast <- as.integer(names(angles[c(i, j)])) # c(1, n)
-
-        theta0 <- mean(atan2(yy[firstandlast], xx[firstandlast]))
-        x0 <- r0 * cos(theta0); y0 <- r0 * sin(theta0)
-        inc <- diff(pretty(c(0, r0))[1:2])
-        srt <- 360*theta0/(2*pi)
-        coef <- -1
-        if (abs(srt) > 90) {
-            srt <- srt + 180
-            coef <- 1
-        }
-        len <- 0.025 * r0 # the length of tick marks
-        r <- r0
-        while (r > 1e-8) {
-            x <- r * cos(theta0); y <- r * sin(theta0)
-            if (len/r < 1) {
-                ra <- sqrt(len^2 + r^2); thetaa <- theta0 + coef * asin(len/r)
-                xa <- ra * cos(thetaa); ya <- ra * sin(thetaa)
-                segments(xa, ya, x, y)
-                text(xa, ya, r0 - r, srt = srt, adj = c(0.5, 1.1), ...)
-            }
-            r <- r - inc
-        }
-        segments(x, y, x0, y0)
+  lastPP <- get("last_plot.phylo", envir = .PlotPhyloEnv)
+  type <- lastPP$type
+  
+  if (type == "unrooted")
+    stop("axisPhylo() not available for unrooted plots; try add.scale.bar()")
+  if (type == "radial")
+    stop("axisPhylo() not meaningful for this type of plot")
+  
+  if (is.null(root.time)) root.time <- lastPP$root.time
+  
+  if (type %in% c("phylogram", "cladogram")) {
+    if(is.null(side)){
+      if (lastPP$direction %in% c("rightwards", "leftwards")) side <- 1
+      else side <- 2
     }
+    if (lastPP$direction %in% c("rightwards", "leftwards")){
+      tmp_range <- range(lastPP$xx)
+      tmp_lim <- lastPP$x.lim
+    } else {
+      tmp_range <- range(lastPP$yy)
+      tmp_lim <- lastPP$y.lim
+    }
+    
+    if (lastPP$direction == "rightwards"){
+      xscale <- c(min(tmp_lim[1],tmp_range[1]), range(lastPP$xx)[2])
+    }
+    if (lastPP$direction == "leftwards"){
+      xscale <- c(tmp_range[1], max(tmp_range[2], tmp_lim[2]))
+    }
+    if (lastPP$direction == "downwards"){
+      xscale <- c(tmp_range[1], max(tmp_range[2], tmp_lim[2]))
+    }  
+    if (lastPP$direction == "upwards"){  
+      xscale <- c(min(tmp_range[1], tmp_lim[1]), tmp_range[2])
+    }
+    
+    tscale <- xscale 
+    tmp <- lastPP$direction %in% c("leftwards", "downwards")
+    if (xor(backward, tmp))  tscale <- tmp_range[2] - tscale  
+    if (!is.null(root.time)) {
+      if(! backward) tscale <- tscale + root.time
+      else tscale <- tscale + root.time - diff(tmp_range)
+    }
+    
+    ## the linear transformation between the x-scale and the time-scale:
+    beta <- diff(xscale) / diff(tscale)
+    alpha <- xscale[1] - beta * tscale[1]
+    lab <- pretty(tscale)
+    x <- beta * lab + alpha
+    axis(side = side, at = x, labels = lab, ...)
+  } else { # type == "fan"
+    n <- lastPP$Ntip
+    xx <- lastPP$xx[1:n]; yy <- lastPP$yy[1:n]
+    r0 <- max(sqrt(xx^2 + yy^2))
+    
+    ## find the widest angle between tips:
+    alpha <- sort(setNames(rect2polar(xx, yy)$angle, 1:n)) # from -pi to +pi
+    angles <- c(diff(alpha), 2*pi - alpha[n] + alpha[1L])
+    j <- which.max(angles)
+    i <- if (j == 1L) n else j - 1L # this is a circle...
+    firstandlast <- as.integer(names(angles[c(i, j)])) # c(1, n)
+    
+    theta0 <- mean(atan2(yy[firstandlast], xx[firstandlast]))
+    x0 <- r0 * cos(theta0); y0 <- r0 * sin(theta0)
+    inc <- diff(pretty(c(0, r0))[1:2])
+    srt <- 360*theta0/(2*pi)
+    coef <- -1
+    if (abs(srt) > 90) {
+      srt <- srt + 180
+      coef <- 1
+    }
+    len <- 0.025 * r0 # the length of tick marks
+    r <- r0
+    while (r > 1e-8) {
+      x <- r * cos(theta0); y <- r * sin(theta0)
+      if (len/r < 1) {
+        ra <- sqrt(len^2 + r^2); thetaa <- theta0 + coef * asin(len/r)
+        xa <- ra * cos(thetaa); ya <- ra * sin(thetaa)
+        segments(xa, ya, x, y)
+        text(xa, ya, r0 - r, srt = srt, adj = c(0.5, 1.1), ...)
+      }
+      r <- r - inc
+    }
+    segments(x, y, x0, y0)
+  }
 }
