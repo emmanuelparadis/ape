247cc1fb533ce31a8dad11c5ed24d5df838bc407
emmanuelparadis <Emmanuel.Paradis@ird.fr>
2024-12-10 21:37:17 +0100

    update for v. 5.8-1

--- R/cophenetic.phylo.R
+++ R/cophenetic.phylo.R
@@ -1,14 +1,19 @@
-## cophenetic.phylo.R (2023-06-07)
+## cophenetic.phylo.R (2024-12-10)
 
 ##   Pairwise Distances from a Phylogenetic Tree
 
-## Copyright 2006-2023 Emmanuel Paradis
+## Copyright 2006-2024 Emmanuel Paradis
 
 ## This file is part of the R-package `ape'.
 ## See the file ../COPYING for licensing issues.
 
-dist.nodes <- function(x)
+dist.nodes <- function(x, fail.if.no.length = FALSE)
 {
+    if (is.null(x$edge.length)) {
+        if (fail.if.no.length) stop("the tree has no branch length")
+        warning("the tree has no branch length: fixing them to one.")
+        x <- compute.brlen(x, 1)
+    }
     x <- reorder(x) # required for the C code
     n <- Ntip(x)
     m <- x$Nnode
