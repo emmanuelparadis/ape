8d8dde5ad546ca00b6c7ca206d8bc5932bfb6941
Klaus Schliep <klaus.schliep@gmail.com>
2025-09-24 18:59:03 +0200

    speed up unique.multiPhylo, add hash

--- R/unique.multiPhylo.R
+++ R/unique.multiPhylo.R
@@ -17,19 +17,27 @@ unique.multiPhylo <-
     if (n == 0L) return(x)
     if (n == 1L) return(structure(x, old.index = 1L))
     keep <- 1L
-    old.index <- seq_len(n)
-    for (i in 2:n) {
-        already.seen <- FALSE
-        for (j in keep) {
-            if (all.equal(x[[j]], x[[i]],
+    
+    tmp <- try(.compressTipLabel(x), TRUE)
+    if(requireNamespace("phangorn") && !inherits(tmp, "try-error") && !use.edge.length && use.tip.label){
+        hash_x <- phangorn::hash(x)
+        keep <- which(!duplicated(hash_x))
+        old.index <- match(hash_x, unique(hash_x))
+    } else {
+        old.index <- seq_len(n)
+        for (i in 2:n) {
+            already.seen <- FALSE
+            for (j in keep) {
+                if (all.equal(x[[j]], x[[i]],
                           use.edge.length = use.edge.length,
                           use.tip.label = use.tip.label)) {
-                already.seen <- TRUE
-                old.index[i] <- j
-                break
+                    already.seen <- TRUE
+                    old.index[i] <- j
+                    break
+                }
             }
+            if (!already.seen) keep <- c(keep, i)
         }
-        if (!already.seen) keep <- c(keep, i)
     }
     res <- x[keep]
     attr(res, "old.index") <- old.index
